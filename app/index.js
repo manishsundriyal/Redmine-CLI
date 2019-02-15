277565f43e6c23c8f25692754bba7db32ff696fe
manish-nx <manish.kumar@successive.tech>
2019-02-15 15:23:17 +0530

    add commander for parsing args

--- app/index.js
+++ app/index.js
@@ -1,10 +1,21 @@
+#!/usr/bin/env node
 const chalk = require('chalk');
 const clear = require('clear');
-const figlet = require('figlet')
+const figlet = require('figlet');
+const commander = require('commander');
 
-clear();
-console.log(
-  chalk.yellow(
-    figlet.textSync("Hello, World", {horizontalLayout: 'full'})
-  )
-);
+commander
+  .command('list')
+  .alias('ls')
+  .description('List anything')
+  .action(() => {
+    clear();
+    console.log(
+      chalk.yellow(
+          figlet.textSync("Welcome", {horizontalLayout: 'full'})
+        )
+      );
+    console.log("App is under development")
+  });
+
+commander.parse(process.argv);
